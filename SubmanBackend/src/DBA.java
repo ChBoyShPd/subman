72f84fd2b3023297d240902145f588285bfe22d1
ChBoyShPd <xuxiaoyao213@gmail.com>
2019-02-22 16:51:53 -0500

    Defined how the back-end would connect to database.

--- SubmanBackend/src/DBA.java
+++ SubmanBackend/src/DBA.java
@@ -1,3 +1,12 @@
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * 
  */
@@ -6,12 +15,55 @@
  * @author xux8
  *
  */
-public class DBA {
-	public DBA() {
+
+public class DBA implements Closeable{
+	static final String URL="jdbc:mysql://work.skylinecfg.org:3306/Subman";
+	static final String USER = "ChBoyShPd"; //FIXME change test username to production 
+	static final String DEFAULTCREDENTIALS = "C:\\\\Users\\\\xux8\\\\SubmanC.txt";
+	private Connection conn;
+
+	public DBA() throws SQLException {
+		String password = "";
+		//get password from file
+		try {
+			BufferedReader credFile = new BufferedReader(new FileReader(DEFAULTCREDENTIALS));
+			password = credFile.readLine();
+			credFile.close();
+		} catch (IOException e) {
+			// TODO log it
+			System.err.println("Credentials read-from-file failed.");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) { //should not be happening
+			System.err.println("PLEASE SETUP THE MYSQL JDBC DRIVER NOW!!!");
+			e.printStackTrace();
+			System.exit(1);
+			e.printStackTrace();
+		}
+		conn=DriverManager.getConnection(URL, USER, password);//would throw when connection fails
+		//TODO log message of successful connection to database
+	}
+
+	/**
+	 * @deprecated
+	 * @param username
+	 * @param password
+	 */
+	public DBA(String username, String password) {
+		// TODO
+	}
+
+	@Override
+	public void close() throws IOException {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// why the fuck would a close() fail
+			e.printStackTrace();
+		}
 		
 	}
-	public DBA(String username, String password) {
-		// TODO 
-	}
 }
-
